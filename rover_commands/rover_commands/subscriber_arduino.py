fa5342194945cc464a12f93f5b95006de5e67e07
Kamil Laachfoubi <kamil.laachfoubi@gmail.com>
2024-11-11 14:51:20 +0100

    changemet usb0 en usb1

--- rover_commands/rover_commands/subscriber_arduino.py
+++ rover_commands/rover_commands/subscriber_arduino.py
@@ -25,7 +25,7 @@ class TrajectorySubscriber(Node):
 
         # Configuration du port série
         try:
-            self.serial_port = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+            self.serial_port = serial.Serial('/dev/ttyUSB1', 9600, timeout=1)
             time.sleep(2)  # Attendre un peu pour que la connexion série se stabilise
             self.get_logger().info('Serial port connected successfully.')
         except serial.SerialException as e:
